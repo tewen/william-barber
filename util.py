2d396117f4f973940cc46e7b2a9145c478f91285
Trevor Ewen <tewen@Trevors-MacBook-Pro-3.local>
2020-04-02 21:15:47 -0400

    Added the util module for the async / sync discrepancy.

---
+++ util.py
@@ -0,0 +1,23 @@
+import asyncio
+import functools
+from concurrent.futures import ThreadPoolExecutor
+
+def force_async(fn):
+    pool = ThreadPoolExecutor()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        future = pool.submit(fn, *args, **kwargs)
+        return asyncio.wrap_future(future)  # make it awaitable
+
+    return wrapper
+
+def force_sync(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        res = fn(*args, **kwargs)
+        if asyncio.iscoroutine(res):
+            return asyncio.get_event_loop().run_until_complete(res)
+        return res
+
+    return wrapper
